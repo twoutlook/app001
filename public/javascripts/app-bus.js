2628d4a0350eccd8e6fc0e48cfa6b2932153d42e
Mark <twcloudwebapp@outlook.com>
2016-01-17 12:18:23 +0800

    dummy ddl can show real min data now

--- public/javascripts/app-bus.js
+++ public/javascripts/app-bus.js
@@ -2,7 +2,23 @@ var app = angular.module("sampleApp", ["firebase"]);
 
 app.controller("SampleCtrl", function($scope, $firebaseArray) {
   var ref = new Firebase("https://bus2016.firebaseio.com/");
-  $scope.messages = $firebaseArray(ref);
 
+  //$scope.messages = $firebaseArray(ref);
+
+  $scope.messages =[
+    {carNum:'001-FY'},
+    {carNum:'001-FP'},
+    {carNum:'002-FQ'},
+
+  ];
+
+
+
+  $scope.messages2={};
+  $scope.check = function (searchText) {
+      console.log("going to use it =>"+searchText);
+      var ref2=ref.child(searchText);
+        $scope.messages2 = $firebaseArray(ref2);
+  }
 
  });
